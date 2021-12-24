dd5b55405d08a5044dc05ac945678d8effbbc47f
Siddharth.Patel <opensiddhu993@gmail.com>
2021-12-24 21:53:29 +0530

    Add Amazone S3 Image Upload

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -6,16 +6,19 @@ import {
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { ApiTags } from '@nestjs/swagger';
+import { S3Service } from './s3service.service';
 import { UploadsService } from './uploads.service';
 
 @Controller('attachments')
 @ApiTags('UploadFiles')
 export class UploadsController {
-  constructor(private readonly uploadsService: UploadsService) {}
+  constructor(private readonly uploadsService: UploadsService,private s3Service: S3Service) {}
 
   @Post()
   @UseInterceptors(FilesInterceptor('attachment[]'))
   uploadFile(@UploadedFiles() attachment: Array<Express.Multer.File>) {
+    console.log(attachment);
+    this.s3Service.uploadFile(attachment[0]);
     return [
       {
         id: '883',
--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UploadsService } from './uploads.service';
 import { UploadsController } from './uploads.controller';
+import { S3Service } from './s3service.service';
 
 @Module({
   controllers: [UploadsController],
-  providers: [UploadsService],
+  providers: [UploadsService,S3Service],
 })
 export class UploadsModule {}
